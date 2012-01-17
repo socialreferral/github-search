84e25cf45a7db200d929efbd5ae37ea59dfd02be
Mark Kremer <mark@without-brains.net>
2012-01-17 16:53:20 +0100

    A little bit more

--- github-search.rb
+++ github-search.rb
@@ -4,8 +4,16 @@ require 'sinatra/base'
 class GitHubSearch < Sinatra::Base
   set :app_file, __FILE__
 
+  get '/' do
+    erb :index, locals: {owner: nil, repository: nil, result: nil}
+  end
+
   get '/:owner/:repository/?*' do
-    erb :index, locals: {owner: params['owner'], repository: params['repository']}
+    result = nil
+    if params['q']
+      result = params['q']
+    end
+    erb :index, locals: {owner: params['owner'], repository: params['repository'], result: result}
   end
 end
 
